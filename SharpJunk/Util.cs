ad61b952be5b69327d8fed88f1ceab3fc835c2b9
arsh <arsh@DESKTOP-RIED9RJ>
2022-08-04 23:55:12 +0300

    moved into scope namespaces

--- SharpJunk/Util.cs
+++ SharpJunk/Util.cs
@@ -1,55 +1,57 @@
 #region
 
+using System;
+using System.Linq;
+
 #endregion
 
 
-namespace SharpJunk;
-
-internal class Util
+namespace SharpJunk
 {
-
-    private static readonly Random rnd = new();
-
-    /// <summary>
-    ///     Generates random class type. <b>class, static class</b>
-    /// </summary>
-    public static string RandomClass()
+    internal class Util
     {
-        string[] ClassArray =
+        private static readonly Random rnd = new();
+
+        /// <summary>
+        ///     Generates random class type. <b>class, static class</b>
+        /// </summary>
+        public static string RandomClass()
         {
+            string[] ClassArray =
+            {
                 "class ",
                 "static class "
-        };
+            };
 
-        int len = rnd.Next(0, ClassArray.Length);
-        return ClassArray[len];
-    }
+            int len = rnd.Next(0, ClassArray.Length);
+            return ClassArray[len];
+        }
 
-    /// <summary>
-    ///     Generates random stuff. <b>Delegate, Random, Void</b>
-    /// </summary>
-    public static string RandomStuff()
-    {
-        string[] stuffs =
+        /// <summary>
+        ///     Generates random stuff. <b>Delegate, Random, Void</b>
+        /// </summary>
+        public static string RandomStuff()
         {
+            string[] stuffs =
+            {
                 $"Random {RandomStr()} = new Random();",
                 $"delegate {RandomVariable()} {RandomStr()}();",
                 $"void {RandomStr()}({RandomVariable()} {RandomStr()}) {{ return; }}"
-        };
+            };
 
-        int len = rnd.Next(0, stuffs.Length);
+            int len = rnd.Next(0, stuffs.Length);
 
-        return stuffs[len];
-    }
+            return stuffs[len];
+        }
 
-    /// <summary>
-    ///     Generates random variable with random type.
-    /// </summary>
-    /// <param name="Nullable">Determines if variable should be nullable.</param>
-    public static string RandomVariable(bool Nullable = false)
-    {
-        string[] variableNames =
+        /// <summary>
+        ///     Generates random variable with random type.
+        /// </summary>
+        /// <param name="Nullable">Determines if variable should be nullable.</param>
+        public static string RandomVariable(bool Nullable = false)
         {
+            string[] variableNames =
+            {
                 "string ",
                 "bool ",
                 "int ",
@@ -60,70 +62,66 @@ internal class Util
                 "float ",
                 "long ",
                 "DateTime "
-        };
+            };
 
-        int result = rnd.Next(0, variableNames.Length);
+            int result = rnd.Next(0, variableNames.Length);
 
-        if (!Nullable) return variableNames[result];
+            if (!Nullable) return variableNames[result];
 
-        if (variableNames[result].Contains("string"))
-        {
-            RandomVariable(true); // "?" cant be applied to string
+            if (variableNames[result].Contains("string"))
+                RandomVariable(true); // "?" cant be applied to string
+            else
+                return variableNames[result] + "?";
+
+            return variableNames[result];
         }
-        else
-        {
-            return variableNames[result] + "?";
-        }
 
-        return variableNames[result];
-    }
-
-    /// <summary>
-    ///     Generates random array with random variable type.
-    /// </summary>
-    public static string AddArray()
-    {
-        string[] variableNames =
+        /// <summary>
+        ///     Generates random array with random variable type.
+        /// </summary>
+        public static string AddArray()
         {
+            string[] variableNames =
+            {
                 $"string[] {RandomStr()} = \n {{\n\n {RandomArrayCount(true)}, \n\n        }}; ",
                 $"int[] {RandomStr()} = \n {{\n\n {RandomArrayCount()}, \n\n        }}; ",
                 $"double[] {RandomStr()} = \n {{\n\n {RandomArrayCount()}, \n\n        }}; ",
                 $"float[] {RandomStr()} = \n {{\n\n {RandomArrayCount()}, \n\n        }}; ",
                 $"decimal[] {RandomStr()} = \n {{\n\n {RandomArrayCount()}, \n\n        }}; ",
                 $"long[] {RandomStr()} = \n {{\n\n {RandomArrayCount()}, \n\n        }}; "
-        };
+            };
 
-        int result = rnd.Next(0, variableNames.Length);
-        return variableNames[result];
-    }
+            int result = rnd.Next(0, variableNames.Length);
+            return variableNames[result];
+        }
 
-    /// <summary>
-    ///     Generates random operator.
-    /// </summary>
-    public static string RandomOperator()
-    {
-        string[] ops =
+        /// <summary>
+        ///     Generates random operator.
+        /// </summary>
+        public static string RandomOperator()
         {
+            string[] ops =
+            {
                 "==",
                 "<=",
                 ">=",
                 "<",
                 ">",
                 "!="
-        };
+            };
 
-        int len = rnd.Next(0, ops.Length);
-        return ops[len];
-    }
+            int len = rnd.Next(0, ops.Length);
+            return ops[len];
+        }
 
-    /// <summary>
-    ///     Generates random using.
-    /// </summary>
-    /// <returns></returns>
-    public static string RandomUsing()
-    {
-        string[] NS =
+        /// <summary>
+        ///     Generates random using.
+        /// </summary>
+        /// <returns></returns>
+        public static string RandomUsing()
         {
+            string[] NS =
+            {
                 "using System.Collections;",
                 "using System.Collections.Specialized;",
                 "using System.Collections.Generic;",
@@ -155,54 +153,54 @@ internal class Util
                 "using System.Timers;",
                 "using System.Deployment.Internal.Isolation.Manifest;",
                 "using System.Diagnostics.Tracing;"
-        };
+            };
 
-        int len = rnd.Next(0, NS.Length);
-        return NS[len];
-    }
-
-    /// <summary>
-    ///     Returns random string.
-    /// </summary>
-    /// <returns></returns>
-    public static string RandomStr()
-    {
-        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        int chance = rnd.Next(10, 30);
-        var stringChars = new char[chance];
-        for ( var i = 0; i < stringChars.Length; i++ ) stringChars[i] = chars[rnd.Next(chars.Length)];
-
-        var finalString = new string(stringChars);
-        return finalString;
-    }
-
-    #region Private
-
-    private static string RandomArrayCount(bool isString = false)
-    {
-        int count = rnd.Next(5);
-
-        if (isString)
-        {
-            var isstringstr = $"\"{RandomStr()}\"";
-
-            for ( var i = 0; i < count; i++ ) isstringstr += $",\"{RandomStr()}\"";
-
-            return isstringstr;
+            int len = rnd.Next(0, NS.Length);
+            return NS[len];
         }
 
-        var onlynumber = $"{RandomNumber()}";
+        /// <summary>
+        ///     Returns random string.
+        /// </summary>
+        /// <returns></returns>
+        public static string RandomStr()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            int chance = rnd.Next(10, 30);
+            var stringChars = new char[chance];
+            for (var i = 0; i < stringChars.Length; i++) stringChars[i] = chars[rnd.Next(chars.Length)];
 
-        for ( var i = 0; i < count; i++ ) onlynumber += $",{RandomNumber()}";
+            var finalString = new string(stringChars);
+            return finalString;
+        }
 
-        return onlynumber;
+        #region Private
+
+        private static string RandomArrayCount(bool isString = false)
+        {
+            int count = rnd.Next(5);
+
+            if (isString)
+            {
+                var isstringstr = $"\"{RandomStr()}\"";
+
+                for (var i = 0; i < count; i++) isstringstr += $",\"{RandomStr()}\"";
+
+                return isstringstr;
+            }
+
+            var onlynumber = $"{RandomNumber()}";
+
+            for (var i = 0; i < count; i++) onlynumber += $",{RandomNumber()}";
+
+            return onlynumber;
+        }
+
+        private static int RandomNumber()
+        {
+            return rnd.Next(int.MaxValue - 99999);
+        }
+
+        #endregion
     }
-
-    private static int RandomNumber()
-    {
-        return rnd.Next(int.MaxValue - 99999);
-    }
-
-    #endregion
-
 }
